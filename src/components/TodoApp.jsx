74d475cd965780ca2bfc4ab0644b43b1dcb9d03a
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-04-03 23:53:50 +0430

    Step 2 completed

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+// other imports here
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
+
+const TodoApp = () => (
+  <>
+    <Header />
+    <TodosLogic />
+  </>
+);
+export default TodoApp;
